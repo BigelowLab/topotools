cca3f41dbf8d35bc6a4c25b54d99dc1a46c96502
Ben Tupper <btupper@bigelow.org>
2023-03-23 09:29:40 -0400

    add sfc to classes detected in as_bb()

--- R/misc.R
+++ R/misc.R
@@ -5,7 +5,7 @@
 #' @return numeric vector of bounding box in [xmin, xmax, ymin, ymax] order
 as_bb <- function(x){
   
-  if (any(inherits(x, c("sf", "stars")))){
+  if (any(inherits(x, c("sfc", "sf", "stars")))){
     x <- (sf::st_bbox(x) |> as.vector())[c(1,3,2,4)]
   } else if (any(inherits(x, c("SpatVector", "SpatRaster")))){
     x <- terra::ext(x) |> unname()
